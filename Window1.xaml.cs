c92603d675be045799705d1ee0c938b947d7faaa
enzo963 <monther2004m5@gmail.com>
2026-03-24 14:35:00 +0300

    Add project files.

---
+++ Window1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using Bio_Athun_System;
+
+namespace BioAthunSystem.Views
+{
+    public partial class LoginWindow : Window
+    {
+        private VideoCapture _capture; // كائن الكاميرا
+        private DispatcherTimer _timer; // مؤقت لتحديث الصورة
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string userName = txtUser.Text;
+            string userPass = txtPass.Password;
+
+          
+            
+            if (string.IsNullOrEmpty(userName) && string.IsNullOrEmpty(userPass))
+            {
+               MessageBox.Show("Enter User Name and Pass plz..");
+            }
+
+            if (userName == "aaa" && userPass == "123")
+            {
+               Window2 form2 = new Window2();
+               form2.Show();
+            }
+            else
+            {
+                
+            }
+
+
+        }
+
+        private void BtnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
